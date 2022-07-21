26e98ceb275370f99aac042ddf0ad051b0aec237
breno-guardia <ctl_bcardoso@uolinc.com>
2022-07-21 15:58:13 -0300

    feat: add lib (date-fns)

--- Client/mobile/src/utils/functions.ts
+++ Client/mobile/src/utils/functions.ts
@@ -1,12 +1,8 @@
 import { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
+import { format } from 'date-fns';
 
 export const dateFormat = (timestamp: FirebaseFirestoreTypes.Timestamp) => {
   if (timestamp) {
-    const date = new Date(timestamp.toDate());
-
-    const day = date.toLocaleDateString('pt-BR');
-    const hour = date.toLocaleTimeString('pt-BR');
-
-    return `${day} às ${hour}`;
+    return format(timestamp.toDate(), "dd/MM/yy 'às' HH:mm:ss");
   }
 };
